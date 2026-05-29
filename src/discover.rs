e90530b5efe7dd844c952ae37bcfeeac2578c660
Nivedh E R <nivedher12@gmail.com>
2026-05-29 22:40:06 +0530

    fix(ip): removed the localHost ip

--- src/discover.rs
+++ src/discover.rs
@@ -11,8 +11,7 @@ pub fn discover_devices() -> Result<Vec<SocketAddr>> {
     println!("Waiting for responses...");
     socket.set_read_timeout(Some(Duration::from_secs(2)))?;
 
-    let _ = socket.send_to(b"DISCOVER_URBANSEND", "255.255.255.255:9999")?;
-    let _ = socket.send_to(b"DISCOVER_URBANSEND", "127.0.0.1:9999")?;
+    socket.send_to(b"DISCOVER_URBANSEND", "255.255.255.255:9999")?;
 
     let mut devices = Vec::new();
     let mut buffer = [0; 1024];
